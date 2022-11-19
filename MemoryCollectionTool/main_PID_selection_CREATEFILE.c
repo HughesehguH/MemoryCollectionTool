7844dbb7dbdee09f2ce47f76772d4df64cb70f7f
HughesehguH <hughesehguh@gmail.com>
2022-11-18 19:47:50 -0600

    Added alternate file creation

---
+++ MemoryCollectionTool/main_PID_selection_CREATEFILE.c
@@ -0,0 +1,398 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define BUFF_SIZE 10000
+#include <stdio.h>
+#include <Windows.h>
+#include <Psapi.h>
+#include <tlhelp32.h>
+#include <tchar.h>
+
+int menu(void);
+HANDLE getProcessList(void);
+BOOL ListProcessModules(DWORD dwPID);
+BOOL ListProcessThreads(DWORD dwOwnerPID);
+BOOL getProcessInfo(HANDLE hProcessSnap, unsigned int PID);
+BOOL getMemoryData(void);
+void printError(TCHAR* msg);
+
+int main(void)
+{
+    HANDLE hProcessSnap;
+	unsigned int selection = 0;
+
+
+	//selection = menu();
+
+    selection = '1';
+	if (selection == '1')
+	{
+		printf("\n[+] Getting the list of processes now!\n");
+        //selection = 0;
+		hProcessSnap = getProcessList();
+        printf("\n[+] Enter PID to get data for: ");
+        scanf("%d", &selection);
+        getProcessInfo(hProcessSnap, selection);
+
+	}
+	else if (selection == '2')
+	{
+		printf("Getting the memory dump\n");
+	}
+	else
+	{
+		printf("Error!\n");
+	}
+
+	return 0;
+}
+
+int menu(void)
+{
+	char choice = 0;
+
+	while (choice == 0)
+	{
+		printf("\n1. Get list of proccesses\n2. Get memory dump\n\nMake a selection: ");
+		choice = getc(stdin);
+		if (choice != '1' && choice != '2')
+		{
+			printf("[-] Error! Invalid Selection!\n");
+			choice = 0;
+		}
+		getc(stdin);
+	}
+
+
+	return choice;
+}
+
+BOOL getProcessInfo(HANDLE hProcessSnap, unsigned int PID)
+{
+    BOOL match_found = FALSE;
+    unsigned char buff[BUFF_SIZE * sizeof(DWORD)] = {0};
+    PROCESSENTRY32 pe32;
+    PROCESSENTRY32 pe32Parent;
+    MODULEENTRY32 me32;
+    HANDLE hModuleSnap = INVALID_HANDLE_VALUE;
+    HANDLE hProcess;
+    HANDLE hParentProcessSnap;
+    FILE* outfile;
+    hParentProcessSnap = hProcessSnap;
+    long unsigned int cBytesRead = 0;
+    DWORD dwPriorityClass;
+
+    //outfile = fopen("MemDump-Debug.txt", "w");
+
+    if (hProcessSnap == INVALID_HANDLE_VALUE)
+    {
+        printError(TEXT("CreateToolhelp32Snapshot (of processes)"));
+        return(FALSE);
+    }
+
+    // Set the size of the structure before using it.
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+    pe32Parent.dwSize = sizeof(PROCESSENTRY32);
+
+    // Retrieve information about the first process,
+    // and exit if unsuccessful
+    if (!Process32First(hProcessSnap, &pe32))
+    {
+        printError(TEXT("Process32First")); // show cause of failure
+        CloseHandle(hProcessSnap);          // clean the snapshot object
+        return(FALSE);
+    }
+    while (pe32.th32ProcessID != PID)
+    {
+        Process32Next(hProcessSnap, &pe32);
+    }
+
+   // Retrieve information about the first process to be able to iterate through
+   // and exit if unsuccessful
+    if (!Process32First(hParentProcessSnap, &pe32Parent))
+    {
+        printError(TEXT("Process32First")); // show cause of failure
+        CloseHandle(hParentProcessSnap);          // clean the snapshot object
+        return(FALSE);
+    }
+    // match_found = FALSE;
+    while (pe32Parent.th32ProcessID != PID)
+    {
+        Process32Next(hParentProcessSnap, &pe32Parent);
+    }
+    //if (match_found)
+    //{
+        _tprintf(TEXT("\n%s\t\t [%d]"), pe32.szExeFile, pe32.th32ProcessID);
+        // Retrieve the priority class.
+        dwPriorityClass = 0;
+        hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
+        //hParentProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ParentProcessID);
+        if (hProcess == NULL)
+            printError(TEXT("OpenProcess"));
+        else
+        {
+            dwPriorityClass = GetPriorityClass(hProcess);
+            if (!dwPriorityClass)
+                printError(TEXT("GetPriorityClass"));
+            CloseHandle(hProcess);
+        }
+
+        _tprintf(TEXT("\n  Process ID        = 0x%08X"), pe32.th32ProcessID);
+        _tprintf(TEXT("\n  Thread count      = %d"), pe32.cntThreads);
+        _tprintf(TEXT("\n  Parent process ID = 0x%08X (%s)"), pe32.th32ParentProcessID, pe32Parent.szExeFile);
+        _tprintf(TEXT("\n  Priority base     = %d"), pe32.pcPriClassBase);
+        if (dwPriorityClass)
+        {
+            _tprintf(TEXT("\n  Priority class    = %d"), dwPriorityClass);
+        }
+        // List the modules and threads associated with this process
+        //ListProcessModules(pe32.th32ProcessID);
+        //ListProcessThreads(pe32.th32ProcessID);
+        hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pe32.th32ProcessID);
+        if (hModuleSnap == INVALID_HANDLE_VALUE)
+        {
+            printError(TEXT("CreateToolhelp32Snapshot (of modules)"));
+            return(FALSE);
+        }
+
+        // Set the size of the structure before using it.
+        me32.dwSize = sizeof(MODULEENTRY32);
+
+        // Retrieve information about the first module,
+        // and exit if unsuccessful
+        if (!Module32First(hModuleSnap, &me32))
+        {
+            printError(TEXT("Module32First"));  // show cause of failure
+            CloseHandle(hModuleSnap);           // clean the snapshot object
+            return(FALSE);
+        }
+        if (Toolhelp32ReadProcessMemory(pe32.th32ProcessID, me32.modBaseAddr, &buff, (BUFF_SIZE * sizeof(DWORD)) - 3, &cBytesRead) == TRUE)
+        {
+            printf("\nBytes read: %d\n", cBytesRead);
+            HANDLE fp;
+            LPDWORD bytesWritten = 0;
+            fp = CreateFileA("TESTING.txt", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+            if (fp != INVALID_HANDLE_VALUE)
+            {
+                if (WriteFile(fp, &buff, (BUFF_SIZE * sizeof(DWORD)) - 3, &bytesWritten, NULL))
+                {
+                    printf("[+] Wrote to file!\n");
+                }
+                else
+                    printf("[-] Error! Couldn't write to file!\n");
+
+            }
+            else
+            {
+                printf("\n[-] Error! Couldn't open file!\n");
+            }
+
+            CloseHandle(fp);
+
+            /*FILE* fp;
+            fp = fopen("MemDump.txt", "wb+");
+
+            for (int i = 0; i < BUFF_SIZE * sizeof(DWORD) - 3; i += 16)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    fprintf(fp, "%02hhx ", (unsigned char)*(buff + j + i));
+                }
+                fprintf(fp, "\n");
+            }
+
+            fclose(fp);
+            */
+        }
+   // }
+   // else
+   // {
+   //     printf("\nError! THGoat.exe not found!\n");
+   // }
+    return TRUE;
+
+
+}
+/*
+BOOL getMemoryData(void)
+{
+
+
+
+
+
+
+}
+*/
+
+// EVERYTHING BELOW IS NOT CURRENTLY USED BY THE MEM DUMPER BUT WILL BE INCLUDED AT A LATER DATE
+
+HANDLE getProcessList()
+{
+    HANDLE hProcessSnap;
+    HANDLE hProcess;
+    PROCESSENTRY32 pe32;
+    DWORD dwPriorityClass;
+
+    // Take a snapshot of all processes in the system.
+    hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hProcessSnap == INVALID_HANDLE_VALUE)
+    {
+        printError(TEXT("CreateToolhelp32Snapshot (of processes)"));
+        return(FALSE);
+    }
+
+    // Set the size of the structure before using it.
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    // Retrieve information about the first process,
+    // and exit if unsuccessful
+    if (!Process32First(hProcessSnap, &pe32))
+    {
+        printError(TEXT("Process32First")); // show cause of failure
+        CloseHandle(hProcessSnap);          // clean the snapshot object
+        return(FALSE);
+    }
+
+    // Now walk the snapshot of processes, and
+    // display information about each process in turn
+    do
+    {
+        //_tprintf(TEXT("\n\n====================================================="));
+        _tprintf(TEXT("\n%s\t\t [%d]"), pe32.szExeFile, pe32.th32ProcessID);
+        //_tprintf(TEXT("\n-------------------------------------------------------"));
+
+        // Retrieve the priority class.
+        dwPriorityClass = 0;
+        hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
+        if (hProcess == NULL)
+            printError(TEXT("OpenProcess"));
+        else
+        {
+            dwPriorityClass = GetPriorityClass(hProcess);
+            if (!dwPriorityClass)
+                printError(TEXT("GetPriorityClass"));
+            CloseHandle(hProcess);
+        }
+
+        //_tprintf(TEXT("\n  Process ID        = 0x%08X"), pe32.th32ProcessID);
+        //_tprintf(TEXT("\n  Thread count      = %d"), pe32.cntThreads);
+        /*_tprintf(TEXT("\n  Parent process ID = 0x%08X"), pe32.th32ParentProcessID);
+        _tprintf(TEXT("\n  Priority base     = %d"), pe32.pcPriClassBase);
+        if (dwPriorityClass)
+            _tprintf(TEXT("\n  Priority class    = %d"), dwPriorityClass);
+            */
+        // List the modules and threads associated with this process
+       // ListProcessModules(pe32.th32ProcessID);
+       // ListProcessThreads(pe32.th32ProcessID);
+
+    } while (Process32Next(hProcessSnap, &pe32));
+
+    //CloseHandle(hProcessSnap);
+    return(hProcessSnap);
+}
+
+
+BOOL ListProcessModules(DWORD dwPID)
+{
+    HANDLE hModuleSnap = INVALID_HANDLE_VALUE;
+    MODULEENTRY32 me32;
+
+    // Take a snapshot of all modules in the specified process.
+    hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwPID);
+    if (hModuleSnap == INVALID_HANDLE_VALUE)
+    {
+        printError(TEXT("CreateToolhelp32Snapshot (of modules)"));
+        return(FALSE);
+    }
+
+    // Set the size of the structure before using it.
+    me32.dwSize = sizeof(MODULEENTRY32);
+
+    // Retrieve information about the first module,
+    // and exit if unsuccessful
+    if (!Module32First(hModuleSnap, &me32))
+    {
+        printError(TEXT("Module32First"));  // show cause of failure
+        CloseHandle(hModuleSnap);           // clean the snapshot object
+        return(FALSE);
+    }
+
+    // Now walk the module list of the process,
+    // and display information about each module
+    do
+    {
+        _tprintf(TEXT("\n\n     MODULE NAME:     %s"), me32.szModule);
+        _tprintf(TEXT("\n     Executable     = %s"), me32.szExePath);
+        _tprintf(TEXT("\n     Process ID     = 0x%08X"), me32.th32ProcessID);
+        _tprintf(TEXT("\n     Ref count (g)  = 0x%04X"), me32.GlblcntUsage);
+        _tprintf(TEXT("\n     Ref count (p)  = 0x%04X"), me32.ProccntUsage);
+        _tprintf(TEXT("\n     Base address   = 0x%08X"), (DWORD)me32.modBaseAddr);
+        _tprintf(TEXT("\n     Base size      = %d"), me32.modBaseSize);
+
+    } while (Module32Next(hModuleSnap, &me32));
+
+    CloseHandle(hModuleSnap);
+    return(TRUE);
+}
+
+BOOL ListProcessThreads(DWORD dwOwnerPID)
+{
+    HANDLE hThreadSnap = INVALID_HANDLE_VALUE;
+    THREADENTRY32 te32;
+
+    // Take a snapshot of all running threads  
+    hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+    if (hThreadSnap == INVALID_HANDLE_VALUE)
+        return(FALSE);
+
+    // Fill in the size of the structure before using it. 
+    te32.dwSize = sizeof(THREADENTRY32);
+
+    // Retrieve information about the first thread,
+    // and exit if unsuccessful
+    if (!Thread32First(hThreadSnap, &te32))
+    {
+        printError(TEXT("Thread32First")); // show cause of failure
+        CloseHandle(hThreadSnap);          // clean the snapshot object
+        return(FALSE);
+    }
+
+    // Now walk the thread list of the system,
+    // and display information about each thread
+    // associated with the specified process
+    do
+    {
+        if (te32.th32OwnerProcessID == dwOwnerPID)
+        {
+            _tprintf(TEXT("\n\n     THREAD ID      = 0x%08X"), te32.th32ThreadID);
+            _tprintf(TEXT("\n     Base priority  = %d"), te32.tpBasePri);
+            _tprintf(TEXT("\n     Delta priority = %d"), te32.tpDeltaPri);
+            _tprintf(TEXT("\n"));
+        }
+    } while (Thread32Next(hThreadSnap, &te32));
+
+    CloseHandle(hThreadSnap);
+    return(TRUE);
+}
+
+void printError(TCHAR* msg)
+{
+    DWORD eNum;
+    TCHAR sysMsg[256];
+    TCHAR* p;
+
+    eNum = GetLastError();
+    FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL, eNum,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
+        sysMsg, 256, NULL);
+
+    // Trim the end of the line and terminate it with a null
+    p = sysMsg;
+    while ((*p > 31) || (*p == 9))
+        ++p;
+    do { *p-- = 0; } while ((p >= sysMsg) &&
+        ((*p == '.') || (*p < 33)));
+
+    // Display the message
+    _tprintf(TEXT("\n  WARNING: %s failed with error %d (%s)"), msg, eNum, sysMsg);
+}
